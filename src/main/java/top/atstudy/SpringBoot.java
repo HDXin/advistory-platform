f85e7ad97c7cd7e5ac8c7e12a68534af458a9440
huangdexin <735513870@qq.com>
2018-07-08 22:20:36 +0800

    pro init

---
+++ src/main/java/top/atstudy/SpringBoot.java
@@ -0,0 +1,18 @@
+package top.atstudy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by admin on 2017/11/15.
+ */
+@SpringBootApplication
+public class SpringBoot {
+    private static final Logger logger = LoggerFactory.getLogger(SpringBoot.class);
+    public static void main(String[] args){
+        logger.info(" ===>> spring boot main ... ");
+        SpringApplication.run(SpringBoot.class, args);
+    }
+}
