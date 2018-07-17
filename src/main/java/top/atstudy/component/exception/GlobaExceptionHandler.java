098dd6bd9044213b5a7491e388ad963ae800c66f
huangdexin <735513870@qq.com>
2018-07-17 23:27:24 +0800

    日志打印

--- src/main/java/top/atstudy/component/exception/GlobaExceptionHandler.java
+++ src/main/java/top/atstudy/component/exception/GlobaExceptionHandler.java
@@ -1,9 +1,12 @@
 package top.atstudy.component.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import sun.rmi.runtime.Log;
 import top.atstudy.component.enums.base.IErrorEnum;
 import top.atstudy.component.enums.http.IError400Enum;
 import top.atstudy.component.enums.http.IError401Enum;
@@ -24,31 +27,31 @@ import java.util.Map;
  */
 @ControllerAdvice
 public class GlobaExceptionHandler {
+    private Logger logger = LoggerFactory.getLogger(GlobaExceptionHandler.class);
 
-    @ExceptionHandler({APIException.class, FrameworkException.class})
-    public ResponseEntity<Map<String,String>> globaException(HttpServletRequest request, HttpServletResponse response, FrameworkException exception){
-
-        IErrorEnum errorEnum = exception.getErrorEnum();
-        Map<String, String> map = new HashMap<>();
-        map.put("timestamp", new Date().toString());
-        map.put("path", request.getRequestURI());
+    @ExceptionHandler({APIException.class, FrameworkException.class, Exception.class})
+    public ResponseEntity<Map<String,String>> globaException(HttpServletRequest request, HttpServletResponse response, Exception exception){
 
         ResponseEntity responseEntity = null;
-        if(errorEnum instanceof IError400Enum){
+        if(exception instanceof APIException
+            || exception instanceof FrameworkException){
+            IErrorEnum errorEnum = ((FrameworkException)exception).getErrorEnum();
+            Map<String, String> map = new HashMap<>();
+            map.put("timestamp", new Date().toString());
+            map.put("path", request.getRequestURI());
             map.put("code", errorEnum.getCode().toString());
             map.put("message", errorEnum.getMessage());
-            responseEntity = new ResponseEntity<Map<String, String>>(map, HttpStatus.BAD_REQUEST);
-        }else if(errorEnum instanceof IError401Enum){
-            map.put("code", errorEnum.getCode().toString());
-            map.put("message", errorEnum.getMessage());
-            responseEntity = new ResponseEntity<Map<String, String>>(map, HttpStatus.UNAUTHORIZED);
-        }else if(errorEnum instanceof IError403Enum) {
-            map.put("code", errorEnum.getCode().toString());
-            map.put("message", errorEnum.getMessage());
-            responseEntity = new ResponseEntity<Map<String, String>>(map, HttpStatus.FORBIDDEN);
-        }else{
-
-            responseEntity = new ResponseEntity<Map<String, String>>(map, HttpStatus.FORBIDDEN);
+            if(errorEnum instanceof IError400Enum){
+                responseEntity = new ResponseEntity<Map<String, String>>(map, HttpStatus.BAD_REQUEST);
+            }else if(errorEnum instanceof IError401Enum){
+                responseEntity = new ResponseEntity<Map<String, String>>(map, HttpStatus.UNAUTHORIZED);
+            }else if(errorEnum instanceof IError403Enum) {
+                responseEntity = new ResponseEntity<Map<String, String>>(map, HttpStatus.FORBIDDEN);
+            }
+            logger.info("==>> {}", map.toString());
+        }else {
+            responseEntity = new ResponseEntity<Map<String, String>>(HttpStatus.INTERNAL_SERVER_ERROR);
+            logger.info("==>> {}", exception.getMessage());
         }
 
         return responseEntity;
